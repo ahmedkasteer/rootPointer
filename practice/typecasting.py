fbcfeae3662916d2ce316d686f86d020ce2c587c
Ahmed Kasteer <ahmedkasteer@gmail.com>
2025-11-25 17:22:12 +0500

    Created a practice folder and added python code files.
    Practice work for Internship.

---
+++ practice/typecasting.py
@@ -0,0 +1,17 @@
+#concatenate both strings together 
+a = "harry"
+b = "bhai"
+print ( a + b )
+
+#typecasting, using int function to convert string into int
+c = "15"
+d = 20
+print (int(c) + d)
+
+#automatic implicit typecasting to prevent dataloss
+e = 1.99
+print (type(e))
+f = 3 
+print (type(f))
+
+print (e + f)

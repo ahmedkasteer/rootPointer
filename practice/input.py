bbed84ae2387e7c2027b0ec8bcd105c54beb80ed
Ahmed Kasteer <ahmedkasteer@gmail.com>
2025-11-26 16:26:42 +0500

    Added input and new calculator

---
+++ practice/input.py
@@ -0,0 +1,11 @@
+a = input("Enter ur name:  ")
+print ("My name is", a)
+
+x = input ("Enter first number: ")
+y = input ("Enter second number: ")
+#takes input always in string just typecast if u want int
+print (x+y)
+#after typecasting
+print("After typecasting")
+print(int(x) + int(y))
+
---
+++ practice/modern calculator.py
@@ -0,0 +1,13 @@
+#-----CALCULATOR-----#
+
+#this calculator takes inputs and then resolves the arithmetic operations applied. 
+print ("Input 2 integers value") 
+a = input ("Enter ur first int:  ")
+b = input ("Enter ur second int:  ")
+
+print("The sum for the ints is: ", int(a) + int(b))
+print("The subtraction for the ints is: ", int(a) - int(b))
+print("The multiplaction of the ints is: ", int(a) * int(b))
+print("The division for the first int by second int is: ", int(a) / int(b))
+print("The exponential for the first int two the second is: ", int(a)**int(b))
+print("The modulus for the first int by second int is: ", int(a) % int(b))

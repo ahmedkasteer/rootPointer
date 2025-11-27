fb30f9562914f2195f407d5a96321ff148a281df
Ahmed Kasteer <ahmedkasteer@gmail.com>
2025-11-27 18:04:46 +0500

    1d lists

---
+++ practice/lists.py
@@ -0,0 +1,32 @@
+#lists is used to store multiple items in single variable 
+#food is a list 
+food = ["pizza", "hamburgers","hotdog","spaghetti"]
+print(food)
+food = ["pizza", "hamburgers","hotdog","spaghetti"]
+print(food[0]) #returns pizza
+
+#now replace index item 
+food [0] = "sushi" 
+print(food) #prints new list of updated items 
+food.append("ice cream") #appends icecream ath the end of the list 
+for i in food:
+    print(i, end=" ")  
+
+print()
+food.remove("hotdog") #removes the hotdog item from the list. 
+for i in food:
+    print(i, end=" ")
+print()
+print("Popping an element")
+print()
+food.pop() #removes last element from the list. 
+print()
+food.insert(0, "cake") #this adds cake to the first element of the list at index 0  
+print(food)
+food.append("rasgulla")
+print(food)
+print()
+print("Sorting list items alphabetically")
+food.sort()
+print(food)
+print()
--- practice/nested loops.py
+++ practice/nested loops.py
@@ -28,12 +28,3 @@
 #         print(symbol, end= " ")
 #     print()
 # print()
-#now creating a pyramid using nested loops 
-n = 5
-for i in range (n):
-    for j in range(i, n):
-        print(" ", end = " ")
-    for k in range(i+1):
-        print("$", end = " ")
-    print()
-

0a5d98e5def45a6884e20b7152ced7f5dd5048f5
Ahmed Kasteer <ahmedkasteer@gmail.com>
2025-11-28 16:21:44 +0500

    Tuples

--- practice/Tuples.py
+++ practice/Tuples.py
@@ -1,5 +1,7 @@
 #tuples are immutable which means they're not changeable we can iterate through them and use a few functions and they're ordered. 
 
+
+#accessing second student's name using for loop 
 student = (("Ahmed", 21, "Male"), ("Moiz", 22, "Male"))
 
 for index in student:

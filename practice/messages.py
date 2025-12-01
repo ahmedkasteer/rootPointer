3f16a55f9b3f75f7699cda027a715635ce4afe79
Ahmed Kasteer <ahmedkasteer@gmail.com>
2025-12-01 16:35:25 +0500

    Adding modular programming 2 files

---
+++ practice/messages.py
@@ -0,0 +1,6 @@
+def hello():
+    print("Hello from the other side!!!!!")
+
+def bye():
+    print("Bye from the other sideeee!!!!")
+    
---
+++ practice/modules.py
@@ -0,0 +1,9 @@
+#modular programming to separate a program into parts. 
+import messages as m1 #gave nickname to my module. 
+m1.hello() #calling functions over from the other module. 
+m1.bye() #calling functions from the m1 module.
+
+
+
+
+

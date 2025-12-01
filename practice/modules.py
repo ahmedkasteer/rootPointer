8ea9db7f586cf1af7b3d4759ef440de868eb92ee
Ahmed Kasteer <ahmedkasteer@gmail.com>
2025-12-01 16:37:45 +0500

    Adding 3 modular programming files.

--- practice/modules.py
+++ practice/modules.py
@@ -1,9 +1,6 @@
 #modular programming to separate a program into parts. 
-import messages as m1 #gave nickname to my module. 
+import messages as m1 #gave nickname to my module.
+#another way ------ from messages import hello,bye
+#another way ------ from messages import * ----> this will import everything but not safe for large files.  
 m1.hello() #calling functions over from the other module. 
 m1.bye() #calling functions from the m1 module.
-
-
-
-
-

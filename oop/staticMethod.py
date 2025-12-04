dccdaaaddcaa1abf0cd94b242b5a948608828f52
Ahmed Kasteer <ahmedkasteer@gmail.com>
2025-12-04 15:56:45 +0500

    Adding Static Methods (General utility functions)

---
+++ oop/staticMethod.py
@@ -0,0 +1,22 @@
+class Employee:
+    def __init__(self, name, position):
+        self.name = name
+        self.position = position
+
+    def get_info(self):
+        return f"{self.name} = {self.position}"
+    
+    @staticmethod               # <------ Belongs to class but not the objects of class we can call it using class Name. 
+    def is_valid_position(position):
+        valid_positions = ["Manager", "Cashier", "Cook", "Janitor"]
+        return position in valid_positions
+
+print(Employee.is_valid_position("Scientist")) # <------ we use static method when calling by Class Name. 
+
+employee1 = Employee ("Eugene", "Manager")
+employee2 = Employee ("Squidward", "Cashier")
+employee3 = Employee ("Spongebob", "Cook")
+
+print(employee1.get_info()) # <----     we use instance method when calling by objects
+print(employee2.get_info())
+print(employee3.get_info())
